5de05c15bc172fa1667d48b3777e4b31926bc14e
YigitGulay <61705564+louissailor@users.noreply.github.com>
2021-03-09 14:10:26 +0300

    Create story.php

---
+++ story/story.php
@@ -0,0 +1,10 @@
+<?php 
+$story = array(
+    "ln1" => "Lorem Ipsum / is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+    "ln2" => "Lorem Ipsum  is simply dummy text of the printing and / typesetting industry. Lorem Ipsum has been the industry's standard",
+    "ln3" => "Lorem Ipsum  is simply dummy text / of the printing and typesetting industry. Lorem Ipsum has / been the industry's standard",
+    "ln4" => "Lorem Ipsum  is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+    "ln5" => "Lorem Ipsum  is simply dummy text of the printing and typesetting industry. Lorem Ipsum / has been the industry's standard"
+
+);
+
